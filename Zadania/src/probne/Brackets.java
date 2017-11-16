93db4d6bf95a9172c4a0c6fcc8d114c41dafe21f
Hanka <h.kowerska@gmail.com>
2017-11-16 19:01:44 +0100

    zadania z codility

---
+++ Zadania/src/probne/Brackets.java
@@ -0,0 +1,39 @@
+package probne;
+
+import java.util.ArrayDeque;
+
+public class Brackets {
+	
+	public static int solution(String S) {
+		ArrayDeque<Character> stack = new ArrayDeque<Character>();
+		for(int i=0; i<S.length();i++) {
+			if(S.charAt(i)=='}' || S.charAt(i)==']' || S.charAt(i)==')') {
+				if(stack.isEmpty()) {
+					return 0;
+				}
+				char top = stack.pop();
+				if((top!='{' && S.charAt(i)=='}') || (top!='[' && S.charAt(i)==']') || (top!='(' && S.charAt(i)==')')) {
+					return 0;	
+				}
+			}else {
+				stack.push(S.charAt(i));
+			}
+		}
+		if(stack.isEmpty()) {
+			return 1;
+		}else {
+			return 0;
+		}
+    } 
+
+	public static void main(String[] args) {
+		String S = "{[()()]}";
+		String T = "([)()]";
+		String U = ")(";
+		System.out.println(solution(S));
+		System.out.println(solution(T));
+		System.out.println(solution(U));
+
+	}
+
+}
